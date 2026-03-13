8f80d09e80418b0ee59f6c625404e49aa12e33d6
aaronreg <89247074+aaronreg@users.noreply.github.com>
2026-03-13 11:59:43 +0000

    Delete test_app.py

--- test_app.py
+++
@@ -1,9 +0,0 @@
-from app import add
-
-def test_add_correct():
-    # This should pass
-    assert add(2, 3) == 5
-
-def test_add_wrong():
-    # This will FAIL to show you how the pipeline reacts
-    assert add(2, 2) == 5 
